569c283cd6ada4aa6004610ac1861e2bb90c1e0c
Ben Clifford <benc@hawaga.org.uk>
2017-08-17 14:07:08 +0100

    implement employee national insurance

--- income-tax.R
+++ income-tax.R
@@ -11,6 +11,11 @@ few_incomes <- c(100,10000, 15000, 45000, 46000, 60000, 149000, 152000, 200000)
 gross_incomes <- seq(1,200000, by=50)
 
 # parameters defined by government:
+
+# INCOME TAX #
+
+# all of these sterling amounts are per annum.
+
 # you get this much tax free:
 personal_allowance <- 11500
 
@@ -28,6 +33,30 @@ additional_rate_threshold <- 150000
 
 # you are taxed at the Additional Rate:
 additional_rate <- 0.45 
+
+
+# NATIONAL INSURANCE #
+
+# these figures are class 1, regular employee amounts
+# The UK government gives these in monthly, not annual, amounts
+# so all of these sterling amounts are per calendar month
+
+# the primary threshold is:
+primary_threshold <- 680
+
+# above the primary threshold
+# to the upper earnings limit
+# you pay:
+primary_rate <- 0.12
+
+# the upper earnings limit is:
+upper_earnings_limit <- 3750
+
+# above the upper earnings limit
+# you pay:
+upper_rate <- 0.02
+
+# End of government parameters #
 
 basic_tax <- function(gross) { 
   max(0, (gross - personal_allowance) * 0.2)
@@ -47,12 +76,30 @@ tax_function <- function(gross) {
 
 taxes_for_incomes <- sapply(gross_incomes, tax_function)
 
+# this doesn't work so well as a cumulation of rates, because the basic
+# rate is 12%, and then higher than that is effectively a -10% on top
+# of that. So will compute it as a single value, rather than tiers.
+national_insurance_basic <- function(gross) {
+  max(0, (gross - (primary_threshold * 12)) * primary_rate)
+}
+
+# this one looks a bit different to the others because it *can* give
+# a negative contribution. max is moved to a different position, that
+# might also work in the others?
+national_insurance_higher <- function(gross) {
+  max(0,(gross - (upper_earnings_limit * 12))) * (upper_rate - primary_rate)
+}
+
+national_insurance <- function(gross) {
+  national_insurance_basic(gross) + national_insurance_higher(gross)
+}
 
 fr <- function(incomes) {
   data.frame(incomes,  
                         sapply(incomes, basic_tax),
                         sapply(incomes, higher_tax),
-                        sapply(incomes, additional_tax)
+                        sapply(incomes, additional_tax),
+                        sapply(incomes, national_insurance)
                        )
 }
 
